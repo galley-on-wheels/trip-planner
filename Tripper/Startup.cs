415afc5b11245150539f168f18df0bb92f271e57
manyak.yuriy <manyak.yuriy@gmail.com>
2017-04-16 12:29:21 +0300

    Initialized the seed project.

---
+++ Tripper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tripper.Startup))]
+namespace Tripper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
