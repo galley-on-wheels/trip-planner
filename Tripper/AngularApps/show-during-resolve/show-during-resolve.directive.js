38d062c5df78e30426088904ae08d85c43b3b339
manyak.yuriy <manyak.yuriy@gmail.com>
2017-05-01 23:08:28 +0300

    Added the Angular controllers for the dasboard tabs.

---
+++ Tripper/AngularApps/show-during-resolve/show-during-resolve.directive.js
@@ -0,0 +1,15 @@
+﻿angular.module('mainApp').directive('showDuringResolve', function ($rootScope) {
+
+    return {
+        link: function (scope, element) {
+
+            element.addClass('ng-hide');
+
+            var unregister = $rootScope.$on('$routeChangeStart', function () {
+                element.removeClass('ng-hide');
+            });
+
+            scope.$on('$destroy', unregister);
+        }
+    };
+});
---
+++ Tripper/AngularApps/tabs/dashboard/dashboardController.js
@@ -0,0 +1,4 @@
+﻿angular.module('mainApp').controller('dashboardController', function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
