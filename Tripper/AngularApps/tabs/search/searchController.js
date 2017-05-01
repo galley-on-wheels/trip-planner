c4bd3b99074c93cce98bfb9b4e0e9f2a6af3e994
manyak.yuriy <manyak.yuriy@gmail.com>
2017-05-02 01:26:25 +0300

    Moved the settingsController to an appropriate place.

--- Tripper/AngularApps/tabs/search/searchController.js
+++ Tripper/AngularApps/tabs/search/searchController.js
@@ -1,1 +1,26 @@
-﻿
+﻿angular.module('mainApp').controller('searchController', function ($scope, $http) {
+
+    $scope.settings = {};
+
+    $http.get("search/GetAvailableCultures")
+    .then(function (response) {
+        $scope.settings.locales = response.data;
+        console.log(JSON.stringify(response.data), false);
+    });
+
+    $http.get("search/GetAvailableCurrencies")
+    .then(function (response) {
+        $scope.settings.currencies = response.data;
+        console.log(JSON.stringify(response.data), false);
+    });
+
+    $scope.loadCountries = function () {
+        $http.get("search/GetAvailableCountries", {
+            params: { locale: $scope.settings.selectedLocale }
+        })
+        .then(function (response) {
+            $scope.settings.countries = response.data;
+            console.log(JSON.stringify(response.data), false);
+        });
+    }
+});
