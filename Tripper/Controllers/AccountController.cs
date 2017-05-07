f265383bbb0e202cbca183d3425da03128eea671
yuriy-manyak <yuriy.manyak@softheme.com>
2017-05-07 05:05:17 +0300

    Redirect to the Landing page after LogOff

--- Tripper/Controllers/AccountController.cs
+++ Tripper/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace Tripper.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Landing");
         }
 
         //
