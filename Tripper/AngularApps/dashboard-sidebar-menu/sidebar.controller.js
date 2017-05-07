99febd72ad98570c05ef265d925668ebcb45b883
yuriy-manyak <yuriy.manyak@softheme.com>
2017-05-07 20:03:36 +0300

    Make the sidebar items selection more intuitive

--- Tripper/AngularApps/dashboard-sidebar-menu/sidebar.controller.js
+++ Tripper/AngularApps/dashboard-sidebar-menu/sidebar.controller.js
@@ -1,5 +1,12 @@
-﻿angular.module('mainApp')
-    .controller('sideBarController',
-        function($scope, $http) {
-            
-        });
+﻿angular.module('mainApp').controller('sideBarController', function ($scope, $http) {
+
+    $scope.selectedMenuItem = "search";
+
+    $scope.selectMenuItem = function(itemToSelect) {
+        $scope.selectedMenuItem = itemToSelect;
+    }
+
+    $scope.isSelected = function(itemToCheck) {
+        return itemToCheck === $scope.selectedMenuItem;
+    }
+});
