2edac8ac75590676bee576ad6cdcda8bc711dc4c
Maksym Tishkov <maksym.tishkov@gmail.com>
2017-05-20 14:41:21 +0300

    Profile edit page + DB

--- Tripper/AngularApps/tabs/profile/profileController.js
+++ Tripper/AngularApps/tabs/profile/profileController.js
@@ -1,3 +1,75 @@
-﻿angular.module('mainApp').controller('profileController', function ($scope) {
+﻿angular.module('mainApp').controller('profileController', function($scope, $http) {
 
+    $scope.settings = {};
+    $scope.isDisabled = true;
+    $scope.buttonTitle = "Edit";
+
+    $http.get("account/GetCurrentUserInfo")
+        .then(function(response) {
+            if (response && response.data) {
+                $scope.accountId = response.data.Id;
+                $scope.lastName = response.data.LastName;
+                $scope.firstName = response.data.FirstName;
+                $scope.middleName = response.data.MiddleName;
+                $scope.email = response.data.Email;
+                $scope.phoneNumber = response.data.PhoneNumber;
+                $scope.settings.selectedLocale = response.data.LocaleCode;
+                if ($scope.settings.selectedLocale) {
+                    $scope.loadCountries();
+                }
+                $scope.settings.selectedCountry = response.data.CountryCode;
+                $scope.settings.selectedCurrency = response.data.CurrencyCode;
+            }
+        });
+
+    $http.get("search/GetAvailableCultures")
+        .then(function (response) {
+            $scope.settings.locales = response.data;
+            console.log(JSON.stringify(response.data), false);
+        });
+
+    $http.get("search/GetAvailableCurrencies")
+        .then(function (response) {
+            $scope.settings.currencies = response.data;
+            console.log(JSON.stringify(response.data), false);
+        });
+
+    $scope.loadCountries = function () {
+        $http.get("search/GetAvailableCountries", {
+                params: { locale: $scope.settings.selectedLocale }
+            })
+            .then(function (response) {
+                $scope.settings.countries = response.data;
+                console.log(JSON.stringify(response.data), false);
+            });
+    }
+    $scope.edit = function() {
+        if ($scope.isDisabled) {
+            $scope.buttonTitle = "Save";
+            $scope.enableEditing();
+        } else {
+            $scope.saveAccount();
+        }
+    }
+    $scope.enableEditing = function() {
+        $scope.isDisabled = false;
+    }
+
+    $scope.saveAccount = function () {
+        var data = {};
+        data.Id = $scope.accountId;
+        data.FirstName = $scope.firstName;
+        data.LastName = $scope.lastName;
+        data.MiddleName = $scope.middleName;
+        data.Email = $scope.email;
+        data.PhoneNumber = $scope.phoneNumber;
+        data.CountryCode = $scope.settings.selectedCountry;
+        data.LocaleCode = $scope.settings.selectedLocale;
+        data.CurrencyCode = $scope.settings.selectedCurrency;
+        $http.post("account/EditAccount", data).then(
+            function (response) {
+                var a = 0;
+            });
+    }
+    
 });
---
+++ Tripper/Models/DataAccess/TripperContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Tripper.Models.DataAccess;
+
+namespace Tripper.Models
+{
+    public class TripperContext: DbContext
+    {
+        public TripperContext() 
+            : base("name=DbConnection") 
+        {
+        }
+
+        public virtual DbSet<AccountInfo> Accounts { get; set; }
+
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+    }
+}
