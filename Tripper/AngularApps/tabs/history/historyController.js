02d20b6be08320fe7e620d991353a449895b1164
manyak.yuriy <manyak.yuriy@gmail.com>
2017-06-10 19:25:53 +0300

    History page new UI + history backend is now based on hashtable, not RDS

--- Tripper/AngularApps/tabs/history/historyController.js
+++ Tripper/AngularApps/tabs/history/historyController.js
@@ -1,3 +1,19 @@
-﻿angular.module('mainApp').controller('historyController', function ($scope) {
+﻿angular.module('mainApp').controller('historyController', function ($scope, $http, $interval) {
 
+    $scope.userHistory = [];
+
+    $scope.sortType = 'OriginPlace';
+
+    var refresh = function() {
+        $scope.refreshHistory();
+    }
+
+    $interval(refresh, 7000);
+
+    $scope.refreshHistory = function() {
+        $http.get('history/GetHistoryRecords')
+          .then(function (response) {
+              $scope.userHistory = response.data;
+          });
+    }
 });
---
+++ Tripper/Controllers/HistoryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Skyscanner.Contracts.BrowseRoutes;
+using Skyscanner.Contracts.BrowseRoutes.Expanded;
+using Tripper.Implementation;
+using Tripper.Models.TripSearch;
+
+namespace Tripper.Controllers
+{
+    public class HistoryController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetHistoryRecords()
+        {
+            List<CreateSessionViewModel> userHistory = new List<CreateSessionViewModel>();
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var id = User.Identity.GetUserId();
+
+                userHistory = HistoryService.GetHistoryRecords(id);
+            }
+
+            var json = Json(userHistory, JsonRequestBehavior.AllowGet);
+
+            return json;
+        }
+    }
+}
